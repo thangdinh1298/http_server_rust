1d9dace340a7e0c042e7a7044e6b688b11a095c5
thangdd <thangdd@coccoc.com>
2022-04-06 13:38:04 +0700

    Added methods for HTTPResponse

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,5 +1,7 @@
+use std::io::Result;
 use std::net::TcpStream;
 use std::collections::HashMap;
+use std::io::Write;
 
 pub struct HTTPResponse {
     message: String,
@@ -20,5 +22,26 @@ impl HTTPResponse {
             headers: HashMap::new(),
             status_and_headers_sent: false,
         }
+    }
+
+    pub fn set_message(&mut self, message: &str) {
+        self.message = message.to_owned();
+    }
+
+    pub fn set_code(&mut self, code: u16) {
+        self.code = code;
+    }
+
+    pub fn set_header(&mut self, key: &str, value: &str) {
+        self.headers.insert(key.to_owned(), value.to_owned());
+    }
+
+    pub fn write_to_body(&mut self, buf: &[u8]) -> Result<usize> {
+        Ok(self.body.write(buf)?)
+    }
+
+    fn write_meta_data(&mut self) -> Result<usize> {
+        let status_line = format!("{} {} {}\r\n", &self.version, self.code, &self.message);
+        Ok(self.body.write(status_line.as_bytes())?)
     }
 }
